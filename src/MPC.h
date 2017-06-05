9f40020ce94d1fa7a6c8c18a2825c16ee41cc027
Michael Krasnyk <michael.krasnyk@gmail.com>
2017-06-05 18:33:53 +0200

    First try

--- src/MPC.h
+++ src/MPC.h
@@ -4,17 +4,21 @@
 #include <vector>
 #include "Eigen-3.3/Eigen/Core"
 
-using namespace std;
+// For converting back and forth between radians and degrees.
+static inline constexpr double pi() { return M_PI; }
+static inline double deg2rad(double x) { return x * pi() / 180; }
+static inline double rad2deg(double x) { return x * 180 / pi(); }
 
-class MPC {
- public:
-  MPC();
+class MPC
+{
+public:
+    MPC();
 
-  virtual ~MPC();
+    virtual ~MPC();
 
-  // Solve the model given an initial state and polynomial coefficients.
-  // Return the first actuatotions.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+    // Solve the model given an initial state and polynomial coefficients.
+    // Return the first actuatotions.
+    std::tuple<double, double, std::vector<double>, std::vector<double>, double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
 };
 
 #endif /* MPC_H */
